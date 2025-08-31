79c99342f60997872356a5f308114b00718099b5
TranNhutThong1999 <49718841+TranNhutThong1999@users.noreply.github.com>
2025-09-01 01:37:34 +0700

    'fix'

--- modules/comments/index.ts
+++ modules/comments/index.ts
@@ -1,8 +1,6 @@
 // Export comments API service
 export { commentsApi } from './services/comments.api';
 
-// Export comments hooks
-export { useComments, useCreateComment, } from './hooks/useComments';
 
 // Export types
 export type { Comment, CreateCommentData } from './types';
